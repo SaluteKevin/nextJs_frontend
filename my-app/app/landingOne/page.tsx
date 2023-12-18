78a287c3d3f5381cdaaa34383e86fe0826a7c53f
SaluteKevin <salute.k@ku.th>
2023-12-18 13:17:42 +0700

    first commit

---
+++ my-app/app/landingOne/page.tsx
@@ -0,0 +1,10 @@
+export default function landingOne(){
+
+    return(
+        <main className="bg-gray-100 h-screen">
+            <div className="text-black">
+                hello nono
+            </div>
+        </main>
+    )
+}
